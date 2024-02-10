39604254e1926d27f87e449a6e92be977ec6107b
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2024-02-10 22:53:15 +0530

    137

---
+++ String mirror.java
@@ -0,0 +1,40 @@
+String mirror - Double strings
+Write a program in the IDE which does the following
+
+Accepts the count of test cases - t - in the 1st line
+First line of each test case consists of a string S
+You need to perform the following operation
+Create a variable X which contains the string S concatenated with the string S
+Output X for each test case
+Sample 1:
+Input
+Output
+3
+ab
+bc
+cd
+abab
+bcbc
+cdcd
+OUTPUT : 
+// Update the '_' in the code below
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        Scanner read = new Scanner(System.in);
+        
+        // accept the count of test cases given in the 1st line
+        int t = read.nextInt();
+        for(int i=0; i<t; i++)
+        {
+            String s = read.next();
+            // create a variable X which stores the value of string S concatenated with itself
+            String x = s + s;
+            // output the variable X
+            System.out.println(x);
+        }
+	}
+}
