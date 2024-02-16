d24c24bed3f498b9b86d362be62ea81472bcfeea
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2024-02-16 22:35:31 +0530

    141

---
+++ Fitness.java
@@ -0,0 +1,47 @@
+Practice problem - Fitness
+Another practice problem for you.
+
+Chef wants to become fit for which he decided to walk to the office and return home by walking.
+It is known that Chef's office is 
+�
+X km away from his home.
+If his office is open on 
+5
+5 days in a week, find the number of kilometres Chef travels through office trips in a week.
+
+Hint
+
+Return trips imply that 
+2
+2*
+�
+X distance is travelled each day
+Sample 1:
+Input
+Output
+2
+1
+3
+10
+30
+  OUTPUT:
+
+// Update the code below to solve the problem
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    Scanner read = new Scanner(System.in);
+			int t = read.nextInt();
+			
+			for(int i=0; i<t; i++){
+			    int a=read.nextInt();
+			
+			    System.out.println(a*10);
+			}
+			
+		
+	}
+}
