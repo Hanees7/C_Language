35fc31fc06c5fcc8291ee7d0351f91b139c925cc
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2024-03-05 23:40:50 +0530

    148

---
+++ Hello user.java
@@ -0,0 +1,11 @@
+Write a program which does the following
+
+Declare a string variable x
+Accept a text user input - the name of the user - and store it in the variable x
+Output and print to the console "Hello" before the user defined name.
+Remember to add a space between "Hello" and x
+Sample 1:
+Input
+Output
+Chef
+Hello Chef
