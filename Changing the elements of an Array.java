422a52ff14910296e919f77a66b69510496cc070
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2024-03-17 22:15:35 +0530

    156

---
+++ Changing the elements of an Array.java
@@ -0,0 +1,21 @@
+To change the value of a specific element, we can refer to the index number.
+
+Task
+Write a program which does the following
+
+The 3rd month in the given list is incorrect
+Update the 3rd month in the given array with the correct one - "Mar"
+Once the 3rd array element is upda
+OUTPUT :
+
+  class Codechef
+{
+	public static void main (String[] args)
+	{
+		String[] mnts = {"Jan", "Feb", " Dec", "Apr"};
+		// update your code below this line
+		mnts[2]="Mar";
+		System.out.println(mnts[2]);
+
+	}
+}
