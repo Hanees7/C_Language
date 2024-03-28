dbbd0faaea3132336330cc2737547b7459d7fcb8
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2024-03-28 21:06:33 +0530

    166

---
+++ Area & Perimeter of Rectangle.js
@@ -0,0 +1,14 @@
+Write a program for the following problem
+
+Let’s consider a rectangle of sides 11 and 13.
+Output the following on separate lines:
+Area of the rectangle having sides as 11 and 13
+Perimeter of the rectangle having sides as 11 and 13
+For any rectangle, the formula for area is length * breadth.
+The formula for perimeter is 2 * (length + breadth)
+OUTPUT :
+// Update the '__'  (underscores) below to solve the problem
+
+console.log(11 *13)        // Area of the rectangle
+
+console.log(2*(11 + 13))    // Perimeter of the rectangle
