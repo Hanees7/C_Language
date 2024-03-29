5f80a3fef365cbac6ab19cc1144db8db8d52a114
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2024-03-29 22:03:57 +0530

    167

---
+++ Variables in JS.js
@@ -0,0 +1,13 @@
+A variable is like a labelled box where you can store data. Imagine you have a box labelled "age" and you put the number 25 in it. In JavaScript, you would do this by writing var age = 25; (one of the ways, we'll learn about rest soon). This means you're storing the number 25 in the variable named age.
+
+Here's the cool part: whenever you use age in your code, JavaScript will remember it is 25. For example, if you write console.log(age), JavaScript will show 25.
+
+Task
+Create a variable in the editor named age and assign the value 25 to it.
+Print the value of age variable using a console statement.
+Notice how we don't use " " (double quotes) when printing variables.
+  OUTPUT :
+//Complete the code (replace the underscore with required value)
+
+var age = 25;
+console.log(age);
