b2c764007756381f919ae9e6eaced7232f047ce4
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2024-03-30 23:53:29 +0530

    168

---
+++ String Datatype.js
@@ -0,0 +1,18 @@
+The string type is used to store a sequence of characters, i.e. text.
+String values can be surrounded by either double quotes or single quotes. JS does not care about whether you use single or double quotes.
+
+For example, both the below codes do the same thing: Declare a variable with value I am using CodeChef.
+
+var sentence1 = "I am using CodeChef";
+var sentence2 = 'I am using CodeChef';
+Task
+Write a program which does the following
+
+Declare two variables a and b
+Assign Learning to a and is fun! to b
+Display the sentence "Learning is fun!" using variables a and b in a single line
+OUTPUT :
+// your code goes here
+const a ="Learning "; 
+const b ="is fun!"
+console.log(a+b)
