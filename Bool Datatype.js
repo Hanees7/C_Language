24c6c123b75ba7d54a20d6bfd5bf5ee0bec5f07f
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2024-03-31 22:19:06 +0530

    169

---
+++ Bool Datatype.js
@@ -0,0 +1,24 @@
+In programming, you often need to make decisions based on whether something is true or false. For this, we have a data type, called Boolean representing just one of these two values - true and false.
+
+You can create a boolean variable like this:
+
+var studying = true;
+console.log(studying);
+
+// Output:
+// true
+Note how we do not have to provide quotes around the value true.
+
+Task
+Complete the given program which should do the following:
+
+Declare two variables x and y
+Assign the value true to x and the value false to y
+Output x and y space separated on a single line
+OUTPUT :
+// Replace the underscores with required values
+
+var x = true;
+var y = false;
+
+console.log(x, y);
