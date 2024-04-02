7fd9c62b228b5be8d371814236b8a4ed5c1482fa
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2024-04-03 00:05:49 +0530

    170

---
+++ Cost of painting.js
@@ -0,0 +1,18 @@
+Write a program which does the following
+
+Find the area of a square of side 14cm.
+Also, find the cost of painting the square if the paint costs is 7$ cm/sq.
+Initialize the following variables in your program and use them in the computation
+s to store the length of side the square
+area to store the area of the square
+cost to store the cost of painting
+Output area and cost to the console
+OUTPUT :
+// your code goes here
+var s = 14;
+
+var area = s * s;
+var cost = area * 7;
+
+console.log(area);
+console.log(cost);
