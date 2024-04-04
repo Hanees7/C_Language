831f2a812e1ea47fed929bd4f353d04ea38dbb1a
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2024-04-04 23:57:15 +0530

    171

---
+++ Negative Numbers.js
@@ -0,0 +1,20 @@
+Negative Numbers
+Write a program which does the following:
+
+Display the sum and product of 2 integers: -50 & 40.
+Initialize the following variables in your program and use them in the computation
+Variable a and assign it the value -50
+Variable b assign it the value 40
+Variables total & product - assign the relevant values to them
+Output total and product as space sep
+
+OUTPUT:
+// your code goes here
+
+var a = -50;
+var b = 40;
+
+var total = a + b;
+var product = a * b;
+
+console.log(total, product);
