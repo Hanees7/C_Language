57ab829a420ac6e89b11bc2c34d3ecfc2ad69439
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2024-04-08 22:26:25 +0530

    175

---
+++ String length.js
@@ -0,0 +1,15 @@
+We can use the length property to get the length of a string.
+For eg.
+
+var greeting = "hello";
+console.log(greeting.length);
+// output is 5
+Task
+Write a program which does the following
+
+Create a variable txt and assign it the text NumeroTres
+Use the length pr
+OUTPUT :
+// your code goes here
+var txt = "NumeroTres";
+console.log(txt.length);
