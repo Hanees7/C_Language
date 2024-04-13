88e78b131f72ece097b5735bc78f1818c412bef5
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2024-04-13 22:12:17 +0530

    179

---
+++ Why we need functions.js
@@ -0,0 +1,30 @@
+Why we need functions
+Write a program to calculate the square and cube of numbers 2, 3, and 4.
+
+Sample 1:
+Input
+Output
+ 
+4
+8
+9
+27
+16
+6
+OUTPUT :
+// Naive solution
+
+var two = 2;
+var three = 3;
+var four = 4;
+
+
+console.log(two * two);
+console.log(two * two * two);
+
+console.log(three * three);
+console.log(three * three * three);
+
+console.log(four * four);
+console.log(four * four * four);
+
