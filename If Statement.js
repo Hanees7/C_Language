37d8d7302ce09271de525390ddf9c2c4f9d3b04f
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2024-04-24 23:03:05 +0530

    182

---
+++ If Statement.js
@@ -0,0 +1,37 @@
+The else statement is optional.
+
+Here is an example
+
+var grade = 95;
+if(grade >= 90){
+    console.log("You got an A");
+}
+
+// Output:
+// You got an A
+In the above program, if the condition is True, then the console statement is executed.
+
+Task
+Write a program which does the following
+
+Take input for two integer inputs
+Output "Coding is Fun" to the console if the first integer is greater than the second one.
+Sample 1:
+Input
+Output
+25
+20
+Coding is Fun
+Sample 2:
+Input
+Output
+20
+20
+ OUTPUT :
+ // Complete the given function to get the desired output
+
+function check(a, b){
+ if(a>b){
+     console.log("Coding is Fun")
+ }
+}
