725000eb5f7fd2af9e64798937e6e1c47de2009f
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2024-04-27 00:22:25 +0530

    184

---
+++ Else Statement.js
@@ -0,0 +1,33 @@
+As evident from previous example, the else keyword includes anything which isn't included in the previous if or else if conditions.
+
+Task
+Some code is written in the editor.
+
+Two variables ram and karan are defined.
+Output based on these conditions:
+If ram is greater than karan, output "Ram is heavier than Karan"
+If ram is less than karan, output "Karan is heavier than Ram"
+Otherwise, output "Ram & Karan have the same weight!"
+Your task is to complete the blanks in the code.
+  OUTPUT :
+var ram = 74;
+var karan = 82;
+
+if (ram > karan) {
+    console.log("Ram is heavier than Karan");
+} else if(ram < karan) {
+    console.log("Karan is heavier than Ram");
+} else {
+    console.log("Ram & Karan have the same weight!");
+}
+    
+ram = 78
+karan = 78
+
+if (ram > karan){
+    console.log("Ram is heavier than Karan");
+} else if(ram < karan) {
+    print("Karan is heavier than Ram");
+} else {
+    console.log("Ram & Karan have the same weight!");
+}
