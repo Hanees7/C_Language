40a288bb99ca5fa8c78b0386422841f7cdffff08
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2024-04-29 23:01:10 +0530

    186

---
+++ Multiple Choice Question.js
@@ -0,0 +1,20 @@
+What will be the output of this code?
+
+var a = 0;
+var b = -10;
+
+if (a >= b) {
+   console.log("a is greater or equal to b.");
+}
+if (a === 0) {
+   console.log("a is 0.");
+}
+if (a <= 5) {
+   console.log("a is not more than 5.");
+}
+console.log("Program ends");
+OUTPUT :
+a is greater or equal to b.
+a is 0.
+a is not more than 5.
+Program ends
