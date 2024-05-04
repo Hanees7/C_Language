fff0a1c3f5ff9007ac88b2bc0c939d8bdaa997be
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2024-05-04 23:57:43 +0530

    191

---
+++ Creating Arrays.js
@@ -0,0 +1,22 @@
+Arrays are a fundamental data structure in JavaScript that allows you to store and organize collections of data. Whether you're dealing with a list of numbers, strings, or more complex objects, arrays provide a convenient way to manage and manipulate your data.
+
+Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+
+In JS, an array is denoted by square brackets [ ]
+
+It can hold elements of different data types, such as numbers, strings, or even other lists.
+Arrays are mutable, which means you can modify them by adding, removing, or changing elements.
+Consider the array 'fruits' and 'numbers' below
+
+var fruits = ["apple", "banana", "orange", "grape"];
+
+var numbers = [20, 10, 0, -5];
+Task
+Write a program which does the following
+
+Create an Array of the first 5 positive integers - name it integers
+Once the array is defined - output "Done" to the console.
+OUTPUT :
+// your code goes here
+var fruits = [1, 2, 3,4 ,5];
+console.log("Done")
