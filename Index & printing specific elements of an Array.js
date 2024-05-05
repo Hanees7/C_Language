f3e5626f3b090a815dab006fa416c381ba0bf403
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2024-05-05 22:41:32 +0530

    192

---
+++ Index & printing specific elements of an Array.js
@@ -0,0 +1,16 @@
+You access an array element by referring to the index number inside square brackets [ ].
+Similar to what we saw in strings, array items are indexed and you can access them by referring to their index number:
+
+            0      1      2      - index numbers
+         ["red","Green","Blue"]  - the array
+Note - Indexing always starts from 0 when going left to right.
+
+Write a program which does the following
+
+Output the 3rd element from the given array to the console.
+OUTPUT:
+// Solution as follows
+
+var nums = [1, 2, 3, 4, 5];
+
+console.log(nums[2]);
