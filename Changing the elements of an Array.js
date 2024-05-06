b7d40ca58e388c24e951d7f14f61219aeb38ad18
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2024-05-06 23:40:22 +0530

    193

---
+++ Changing the elements of an Array.js
@@ -0,0 +1,16 @@
+You can also modify elements in an array by assigning a new value to a specific index.
+
+var fruits = ["apple", "banana", "orange", "grape"]
+fruits[1] = "kiwi"
+console.log(fruits);  // Output: ["apple", "kiwi", "orange", "grape"]
+Task
+Write a program which does the following.
+
+The 3rd month in the given array is incorrect.
+Update the 3rd month in the given array with the correct one - "Mar".
+Once the 3rd array element is updated, output the entire array to the console.
+OUTPUT :
+// Update the 3rd month in the list below to "Mar"
+
+var months = ["Jan", "Feb", "Mar", "Apr"];
+console.log(months)
