cd61be014721bdc1a59232b7144cf4d0e00662a8
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2024-05-07 23:03:29 +0530

    194

---
+++ Displaying elements.js
@@ -0,0 +1,10 @@
+Write a program which does the following
+
+Create a string array for the following values "Monday", "Tuesday", "Wednesday", and "Thursday".
+Name the list as per your choice
+Output the last two elements of the array on separate lines
+OUTPUT:
+// your code goes here
+var a=["Monday", "Tuesday", "Wednesday", "Thursday"]
+console.log(a[2]);
+console.log(a[3]);
