8cf295af4ed14880d759180f8db29eb0aa7cee78
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2024-05-08 22:35:17 +0530

    195

---
+++ Displaying the count of elements.jsx
@@ -0,0 +1,14 @@
+Same as what we saw in string, to get the length of an array or the number of elements present in an array, you can use the length property of the array:\
+
+For e.g.
+
+var myNumbers = [10, 20, 30, 40, 50];
+console.log(myNumbers.length);      // will output 5
+Write a program which does the following
+
+Create an int array containing the elements - 10, 20, 30, 40, 50, 60
+Compile and output to the console the accurate count of the number of integer elements in the given array.
+OUTPUT :
+// your code goes here
+var arr=[10, 20, 30, 40 ,50, 60];
+console.log(arr.length);
